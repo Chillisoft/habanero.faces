6d2235928ef96a438130f3efca272046f0010ca9
Chillisoft <support@chillisoft.co.za>
2011-01-11 21:03:17 +0000

    h0h0 this is going to break so much stuff ;_:
    git-svn-id: http://delicious:8080/svn/habanero/HabaneroCommunity/Faces/tags/v2.4.0-StMarys@14914 6b8f5710-b20c-734b-ac0a-461ee9249d7d

---
+++ source/Habanero.Faces.Test.VWG/Menu/TestCollapsiblePanelGroupControlVWG.cs
@@ -0,0 +1,19 @@
+﻿using Habanero.Faces.Test.Base;
+using Habanero.Faces.Base;
+using NUnit.Framework;
+
+namespace Habanero.Faces.Test.VWG.Menu
+{
+    /// <summary>
+    /// This test class tests the CollapsiblePanelGroupControl for VWG.
+    /// </summary>
+    [TestFixture]
+    public class TestCollapsiblePanelGroupControlVWG : TestCollapsiblePanelGroupControl
+    {
+        protected override IControlFactory GetControlFactory()
+        {
+            GlobalUIRegistry.ControlFactory = new Habanero.Faces.VWG.ControlFactoryVWG();
+            return GlobalUIRegistry.ControlFactory;
+        }
+    }
+}
---
+++ source/Habanero.Faces.Test.Win/StandardControls/TestSplitContainerWin.cs
@@ -0,0 +1,19 @@
+﻿using Habanero.Faces.Test.Base;
+using Habanero.Faces.Base;
+using Habanero.Faces.Win;
+using NUnit.Framework;
+
+namespace Habanero.Faces.Test.Win.StandardControls
+{
+    /// <summary>
+    /// This test class tests the SplitContainer class.
+    /// </summary>
+    [TestFixture]
+    public class TestSplitContainerWin : TestSplitContainer
+    {
+        protected override IControlFactory GetControlFactory()
+        {
+            return new ControlFactoryWin();
+        }
+    }
+}
