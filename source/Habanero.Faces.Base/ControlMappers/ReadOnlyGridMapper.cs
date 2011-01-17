53cee8e6dec48c89a4d012092e5654811fd20bb0
Chillisoft <support@chillisoft.co.za>
2011-01-17 18:01:16 +0000

    Fixed #1480 (ControlMappers: Create ReadOnlyGridMapper for mapping children relationships to a grid selector)
    git-svn-id: http://delicious:8080/svn/habanero/HabaneroCommunity/Faces/trunk@15078 6b8f5710-b20c-734b-ac0a-461ee9249d7d

---
+++ source/Habanero.Faces.Base/ControlMappers/ReadOnlyGridMapper.cs
@@ -0,0 +1,49 @@
+namespace Habanero.Faces.Base
+{
+    /// <summary>
+    /// Maps a multiple relationship collection onto an <see cref="IEditableGridControl"/>
+    /// This is used for editing a Many relationship e.g. If an Invoice has many items
+    /// then this control mapper can be used to provide an editable grid of Invoice Items.
+    /// </summary>
+    public class ReadOnlyGridMapper : ControlMapper
+    {
+        private readonly IReadOnlyGrid _readOnlyGrid;
+        private readonly IGridInitialiser _gridInitialiser;
+
+        /// <summary>
+        /// Constructor for the mapper.
+        /// </summary>
+        /// <param name="ctl">The IEditableGridControl</param>
+        /// <param name="relationshipName">This is the relationship name to use - this relationship must be a multiple relationship and exist on the BusinessObject</param>
+        /// <param name="isReadOnly">Whether the editable grid should be read only or not. Ignored</param>
+        /// <param name="factory">The control factory to use</param>
+        public ReadOnlyGridMapper(IReadOnlyGrid ctl, string relationshipName, bool isReadOnly, IControlFactory factory)
+            : base(ctl, relationshipName, isReadOnly, factory)
+        {
+            _readOnlyGrid = ctl;
+            _gridInitialiser = new GridBaseInitialiser(ctl, factory);
+        }
+
+        /// <summary>
+        /// Updates the properties on the represented business object
+        /// </summary>
+        public override void ApplyChangesToBusinessObject() { }
+
+        /// <summary>
+        /// Updates the value on the control from the corresponding property
+        /// on the represented <see cref="IControlMapper.BusinessObject"/>
+        /// </summary>
+        protected override void InternalUpdateControlValueFromBo()
+        {
+            if (_businessObject != null)
+            {
+                var relationship = _businessObject.Relationships[PropertyName];
+                _gridInitialiser.InitialiseGrid(relationship.RelatedObjectClassDef);
+                _readOnlyGrid.BusinessObjectCollection =_businessObject.Relationships.GetRelatedCollection(PropertyName);
+            } else
+            {
+                _readOnlyGrid.BusinessObjectCollection = null;
+            }
+        }
+    }
+}
