fd588290b71387209dadbbd35b493a64fcb88b57
Brett Powell <brett.powell@chillisoft.co.za>
2011-04-11 16:29:08 +0000

    Added faces.Cf.tests
    git-svn-id: http://delicious:8080/svn/habanero/HabaneroCommunity/Faces/branches/V2.6-CF_Stargate@18034 6b8f5710-b20c-734b-ac0a-461ee9249d7d

---
+++ source/Habanero.Faces.CF.Tests/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Habanero.Faces.CF.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TestClassName
+    {
+        // ReSharper disable InconsistentNaming
+
+    }
+}
