a8b5e0f2e7da95c71447d43d5a3f189867083657
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2012-05-28 11:50:05 +0000

    * get the rakefile to push out nuget packages & get dependencies from Nuget
    git-svn-id: http://delicious:8080/svn/habanero/HabaneroCommunity/Faces/branches/v2.7@28150 6b8f5710-b20c-734b-ac0a-461ee9249d7d

--- Rakefile.rb
+++ Rakefile.rb
@@ -39,13 +39,16 @@ $solution = "source/Habanero.Faces - 2010.sln"
 #---------------------------------TASKS----------------------------------------
 
 desc "Runs the build all task"
-task :default => [:build_all]
+task :default => [:build_all_nuget]
 
 desc "Rakes habanero+smooth, builds Faces"
-task :build_all => [:create_temp, :rake_habanero, :rake_smooth, :build, :delete_temp]
+task :build_all_nuget => [:create_temp, :installNugetPackages, :rake_habanero, :rake_smooth, :build, :delete_temp]
 
 desc "Builds Faces, including tests"
-task :build => [:clean, :updatelib, :msbuild, :test, :commitlib]
+task :build => [:clean, :updatelib, :installNugetPackages, :msbuild, :test, :commitlib]
+
+desc "Pushes Faces to Nuget"
+task :nuget => [:publishFacesBaseNugetPackage, :publishFacesVWGNugetPackage, :publishFacesWinNugetPackage]
 
 #------------------------build Faces  --------------------
 
@@ -104,4 +107,41 @@ end
 svn :commitlib do |s|
 	puts cyan("Commiting lib")
 	s.parameters "ci lib -m autocheckin"
+end
+
+desc "Install nuget packages"
+getnugetpackages :installNugetPackages do |ip|
+    ip.package_names = ["Habanero.Base.V2.6", \
+                        "Habanero.BO.V2.6", \
+                        "Habanero.Console.V2.6", \
+                        "Habanero.DB.V2.6", \
+                        "Habanero.Smooth.v1.6", \
+                        "Habanero.Naked.v1.6", \
+                        "Habanero.Testability.v1.3", \
+                        "Habanero.Testability.Helpers.v1.3", \
+                        "Habanero.Testability.Testers.v1.3"]
+end
+
+desc "Publish the Habanero.Faces.Base nuget package"
+pushnugetpackages :publishFacesBaseNugetPackage do |package|
+  package.InputFileWithPath = "bin/Habanero.Faces.Base.dll"
+  package.Nugetid = "Habanero.Faces.Base.V2.6_2011-08-24"
+  package.Version = "2.6"
+  package.Description = "Habanero.Faces.Base"
+end
+
+desc "Publish the Habanero.Faces.VWG nuget package"
+pushnugetpackages :publishFacesVWGNugetPackage do |package|
+  package.InputFileWithPath = "bin/Habanero.Faces.VWG.dll"
+  package.Nugetid = "Habanero.Faces.VWG.V2.6_2011-08-24"
+  package.Version = "2.6"
+  package.Description = "Habanero.Faces.VWG"
+end
+
+desc "Publish the Habanero.Faces.Win nuget package"
+pushnugetpackages :publishFacesWinNugetPackage do |package|
+  package.InputFileWithPath = "bin/Habanero.Faces.Win.dll"
+  package.Nugetid = "Habanero.Faces.Win.V2.6_2011-08-24"
+  package.Version = "2.6"
+  package.Description = "Habanero.Faces.Win"
 end
