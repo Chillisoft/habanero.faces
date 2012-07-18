73f0319a411460b87ba1b8ca8ef526112d690b0b
Chillisoft <support@chillisoft.co.za>
2012-07-18 11:48:18 +0000

    * remove Enabled from interface -- this was a holdover from the older async code and causes issues with other code ):
    git-svn-id: http://delicious:8080/svn/habanero/HabaneroCommunity/Faces/trunk@28931 6b8f5710-b20c-734b-ac0a-461ee9249d7d

--- source/Habanero.Faces.Base/Async/ISupportAsyncLoadingBase.cs
+++ source/Habanero.Faces.Base/Async/ISupportAsyncLoadingBase.cs
@@ -5,7 +5,6 @@ namespace Habanero.Faces.Base.Async
     public interface ISupportAsyncLoadingBase
     {
         void ExecuteOnUIThread(Delegate method);
-        bool Enabled { get; set; }
         EventHandler OnAsyncOperationComplete { get; set; }
         EventHandler OnAsyncOperationStarted { get; set; }
     }
