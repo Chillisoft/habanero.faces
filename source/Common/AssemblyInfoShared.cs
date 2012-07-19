35fb524948c8d857b0d2342dde2de912205cb0e9
Andrew Russell <andrew.russell@chillisoft.co.za>
2012-07-19 07:56:29 +0000

    removed svn revision number from assembly version revision number
    git-svn-id: http://delicious:8080/svn/habanero/HabaneroCommunity/Faces/branches/v2.7-13_02_2012@28953 6b8f5710-b20c-734b-ac0a-461ee9249d7d

--- source/Common/AssemblyInfoShared.cs
+++ source/Common/AssemblyInfoShared.cs
@@ -36,6 +36,5 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// changed revision number to svn revision number @ point branch was created
-[assembly: AssemblyVersion("2.7.0.22713")]
-[assembly: AssemblyFileVersion("2.7.0.22713")]
+[assembly: AssemblyVersion("2.7.0.0")]
+[assembly: AssemblyFileVersion("2.7.0.0")]
