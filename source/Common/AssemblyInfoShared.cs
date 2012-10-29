3fdc6bfbd5333bef27598476d71485da2b231eae
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2012-10-29 10:55:08 +0000

    * change assembly versions from 9.9.9.999 to 3.1.0.000
    git-svn-id: http://delicious:8080/svn/habanero/HabaneroCommunity/Faces/branches/v3.0@29429 6b8f5710-b20c-734b-ac0a-461ee9249d7d

--- source/Common/AssemblyInfoShared.cs
+++ source/Common/AssemblyInfoShared.cs
@@ -36,5 +36,5 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("9.9.9.999")]
+[assembly: AssemblyVersion("3.1.0.000")]
 [assembly: AssemblyFileVersion("9.9.9.999")]
